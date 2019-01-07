f696322b4a2739e18c49bbfca909daa6bb0040b2
resma1 <37348673+resma1@users.noreply.github.com>
2019-01-07 15:15:43 -0600

    Add files via upload

---
+++ Tarea5.php
@@ -0,0 +1,45 @@
+<?php
+class Animal {
+   
+}
+    class PECES extends Animal{
+		
+     }
+        class tiburones extends PECES{
+			public $tipo_piel;
+			public $tipo_esqueleto;
+			public $tamano;
+			public $tipo_respiracion;
+			
+         }
+        class payaso extends PECES{
+			public $tipo_piel;
+			public $tipo_esqueleto;
+			public $tamano;
+			public $tipo_respiracion;
+         }
+        class balleneas extends PECES{
+			public $tipo_piel;
+			public $tipo_esqueleto;
+			public $tamano;
+			public $tipo_respiracion;
+         }
+
+    class REPTILES extends Animal{
+		
+     }
+
+
+    class AVE extends Animal{
+    
+     }
+
+         class Ganso extends AVE{
+	 
+           }
+         class Pato extends AVE{
+	
+           }
+          class Gallina extends AVE{
+			  
+		  }
